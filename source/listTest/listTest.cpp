b872428d77ba5ffbe682fff26fdb8d211dadcba2
xxxxxyt <xieyut0ng@foxmail.com>
2017-04-16 13:28:15 +0800

    complete list.hpp

--- source/listTest/listTest.cpp
+++ source/listTest/listTest.cpp
@@ -3,34 +3,30 @@
 #include <string>
 #include <fstream>
 #include "getPath.hpp"
-//#include "list.hpp"
+#include "list.hpp"
 
 int main(int argc, char *argv[]) {
 	
 	std::string fileName = sjtu::getPath(argv[0]) + "test.bin";
 	
-	int n = 30;
-	std::fstream io;
-	io.open(fileName, std::fstream::out);
-	io.write(reinterpret_cast<const char *> (&n), sizeof(int));
-	for(int i = 0; i < n; ++i)
-		io.write(reinterpret_cast<const char *> (&i), sizeof(int));
-	io.close();
-	io.open(fileName, std::fstream::in);
-	io.read(reinterpret_cast<char *> (&n), sizeof(int));
-	std::cout << n << std::endl;
-	for(int i = 0, x; i < n; ++i) {
-		io.read(reinterpret_cast<char *> (&x), sizeof(int));
-		std::cout << x << std::endl;
-	}
-	// sjtu::list<int> lst;
-	// lst.readIn(fileName);
-	// std::cout << lst.back();
-	// for(int i = 0; i < 30; ++i) lst.push_back(i);
-	// for(int i = 0; i < 30; ++i) {
-		// std::cout << lst.back() << std::endl;
-		// lst.pop_back();
+	// int n = 30;
+	// std::fstream io;
+	// io.open(fileName, std::fstream::out);
+	// io.write(reinterpret_cast<const char *> (&n), sizeof(int));
+	// for(int i = 0; i < n; ++i)
+		// io.write(reinterpret_cast<const char *> (&i), sizeof(int));
+	// io.close();
+	// io.open(fileName, std::fstream::in);
+	// io.read(reinterpret_cast<char *> (&n), sizeof(int));
+	// std::cout << n << std::endl;
+	// for(int i = 0, x; i < n; ++i) {
+		// io.read(reinterpret_cast<char *> (&x), sizeof(int));
+		// std::cout << x << std::endl;
 	// }
+	sjtu::list<int> lst;
+	lst.readIn(fileName);
+	std::cout << lst.back();
+	// for(int i = 30; i < 60; ++i) lst.push_back(i);
 	// lst.writeOut(fileName);
 	
 	return 0;
