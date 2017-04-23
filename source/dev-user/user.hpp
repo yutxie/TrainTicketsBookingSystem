4e7c893e8ca212432f413a370464f9f2169dd52e
AimAlex <aimalex@lijiangbeideMacBook-Pro.local>
2017-04-23 09:01:16 +0800

    User

--- source/dev-user/user.hpp
+++ source/dev-user/user.hpp
@@ -13,34 +13,53 @@
 namespace sjtu {
     class user {
     private:
-        int userId;
+        std::string userId;
         std::string userName;
-        int password;
+        std::string password;
         int identity;
-        sjtu::list <sjtu::ticket> bookedTicket;
+        sjtu::list <sjtu::ticket> ticketList;
     public:
         user(){};
         
-        user(int _id, const std::string &_name, int _password, int _identity, const sjtu::list <sjtu::ticket> &_book): userId(_id), userName(_name), password(_password), identity(_identity), bookedTicket(_book){}
+        user(const std::string _id, const std::string &_name, const std::string &_password = "000000"): userId(_id), userName(_name), password(_password)){};
         
         user(const user &other) {
             userId = other.userId;
             userName = other.userName;
             password = other.password;
-            identity = other.identity;
-            bookedTicket = other.bookedTicket;
         }
         
         user &operator=(const user &other) {
             userId = other.userId;
             userName = other.userName;
             password = other.password;
-            identity = other.identity;
-            bookedTicket = other.bookedTicket;
         }
         
-        void modifyUserName(const std::string &_name) {
-            userName = _name;
+        const std::string & getId()const {
+            return userId;
+        }
+        
+        const std::string & getName()const {
+            return userName;
+        }
+        
+        const std::string & getPassword()const {
+            return password;
+        }
+        
+        void getTicketList(std::ostream &os) const {
+            for(int i = 0; i < ticketList.size(); ++i) {
+                os << "ticketList: " << ticketList[i].getId() << " ";
+            }
+            os << std::endl;
+        }
+        
+        //较难实现
+        const ticket & getTicket(const std::string &ticketId)const {
+            for()
+        }
+        void modifyName(const std::string &newName) {
+            userName = newName;
             return;
         }
         
@@ -49,6 +68,9 @@ namespace sjtu {
             return;
         }
         
+        void orderTicket(const ticket &tk) {
+            
+        }
         friend std::ostream& operator <<(std::ostream &os, const user &obj) {
             os << "userId: " << obj.userId << std::endl;
             os << "userName: " << obj.userName << std::endl;
